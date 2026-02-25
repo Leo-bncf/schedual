8bac933f0fabe4f7843c245c6d78e74c4d7e4b7c
Leo B <leo.bancroft34@icloud.com>
2026-02-25 00:33:47 +0000

    File changes

--- src/pages/Schedules.jsx
+++ src/pages/Schedules.jsx
@@ -708,7 +708,7 @@ export default function Schedules() {
                     dayStartTime={school?.day_start_time || '08:00'}
                     dayEndTime={school?.day_end_time || '18:00'}
                     periodDurationMinutes={school?.period_duration_minutes || 60}
-                    scheduleSettings={school}
+                    scheduleSettings={getProcessedSettings}
                     globalView={overviewFilterType === 'all'}
                     onSlotClick={(day, uiRow, actionData) => {
                       if (actionData.action === 'move') {
